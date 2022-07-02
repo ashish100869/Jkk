628833d6fde4b572b837ce65149c7e810c4e510a
Ashish Kumar <ashish100869@gmail.com>
2022-07-02 18:21:08 +0530

    responsive design

--- dance classes/index.js
+++ dance classes/index.js
@@ -1,29 +1,87 @@
 // ================scroll nav====================
 var navbar = document.getElementById("navbar");
-    var menus = document.getElementById("menus");
+var menus = document.getElementById("menus");
 
-    window.onscroll = function(){
-        if(window.pageYOffset >= menus.offsetTop){
-            navbar.classList.add("sticky");
-        }else{
-            navbar.classList.remove("sticky");
-        }
-    }
+window.onscroll = function () {
+	if (window.pageYOffset >= menus.offsetTop) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
+};
 
-    // ====================swiper ----------------------
-    const swiper = new Swiper('.swiper', {
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
+// ====================swiper ----------------------
+const swiper = new Swiper(".swiper", {
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
 
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+console.log(window.outerWidth);
+const swiper2 = new Swiper(".mySwiper", {
+	slidesPerView: Math.floor(window.outerWidth / 250),
+	loop: true,
+	freehand: true,
+	autoplay: {
+		delay: 1500,
+		disableOnInteraction: false,
+	},
+	spaceBetween: 15,
+});
+
+const toggleNav = () => {
+	const menuItem = document.querySelectorAll(".menu-responsive");
+	const menus = document.querySelector("#menus");
+	if (menuItem[0].classList.contains("d-block")) {
+		menuItem.forEach((item) => {
+			item.classList.remove("d-block");
+		});
+		menus.style.height = "70px";
+	} else {
+		menus.style.height = "600px";
+		setTimeout(() => {
+			menuItem.forEach((item) => {
+				item.classList.add("d-block");
+			});
+		}, 500);
+	}
+};
+
+// Play one video at once
+const allVideos = document.querySelectorAll("video");
+allVideos.forEach((video) => {
+	video.addEventListener("play", () => {
+		allVideos.forEach((otherVideo) => {
+			if (otherVideo != video) {
+				otherVideo.pause();
+			}
+		});
+	});
+});
+
+// Modal
+const openModal = (e) => {
+	console.log(e.target);
+	const modal = document.querySelector(".modal");
+	modal.style.display = "flex";
+	modalImage = document.querySelector("#modalImage");
+	modalImage.src = e.target.src;
+};
+const closeModal = () => {
+	const modal = document.querySelector(".modal");
+	modal.style.display = "none";
+};
+
+document.querySelectorAll(".imge").forEach((item) => {
+	item.addEventListener("click", openModal);
+});
